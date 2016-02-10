f8e41ba68d9280feda924c0c518202d9c084c41e
manzoli2122 <manzoli2122@gmail.com>
2016-02-10 14:26:37 -0200

    curso realizado atualizacaoo

--- Sae/src/sae/core/application/ManageCursoRealizadoServiceBean.java
+++ Sae/src/sae/core/application/ManageCursoRealizadoServiceBean.java
@@ -4,33 +4,46 @@ import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-
 import br.ufes.inf.nemo.util.ejb3.persistence.BaseDAO;
 import sae.core.domain.CursoRealizado;
 import sae.core.persistence.CursoRealizadoDAO;
 
 
 
-
+/**
+ * Stateless session bean implementing the "Manage CursoRealizado" use case component. See the implemented interface
+ * documentation for details.
+ * 
+ * @author Bruno Manzoli (manzoli2122@gmail.com)
+ * @see sae.core.application.ManageAdministradorService
+ */
 @Stateless
-@DeclareRoles({"Admin", "egresso"})
+@DeclareRoles({"Admin", "egresso", "guest"})
 @RolesAllowed({ "Admin" })
 public class ManageCursoRealizadoServiceBean extends CrudServiceBean<CursoRealizado> implements ManageCursoRealizadoService{
 
 	
-	
+	/** Serialization id. */
 	private static final long serialVersionUID = 1L;
 	
+	
+	/** The DAO for CursoRealizado objects. */
 	@EJB    	
 	private CursoRealizadoDAO cursoRealizadoDAO;
 	
 	
 	
+	
+	/** @see br.ufes.inf.nemo.util.ejb3.application.CrudService#getDAO() */
 	@Override
 	public BaseDAO<CursoRealizado> getDAO() {
 		return cursoRealizadoDAO;
 	}
 
+	
+	
+	
+	/** @see sae.core.application.CrudServiceBean#createNewEntity() */
 	@Override
 	protected CursoRealizado createNewEntity() {
 		return new CursoRealizado();
--- Sae/src/sae/core/control/ManageCursoRealizadoControl.java
+++ Sae/src/sae/core/control/ManageCursoRealizadoControl.java
@@ -1,5 +1,8 @@
 package sae.core.control;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
@@ -9,17 +12,41 @@ import br.ufes.inf.nemo.util.ejb3.application.CrudService;
 import br.ufes.inf.nemo.util.ejb3.controller.CrudController;
 import sae.core.domain.CursoRealizado;
 
+
+/**
+ * Controller class responsible for mediating the communication between user interface and application service for the
+ * use case "Manage CursoRealizado".
+ * 
+ * This use case is a CRUD and, thus, the controller also uses the mini CRUD framework for EJB3.
+ * 
+ * @author Bruno Manzoli (manzoli2122@gmail.com)
+ */
 @Named
 @SessionScoped
 public class ManageCursoRealizadoControl extends CrudController<CursoRealizado> {
 
+		
+	/** Serialization id. */
 	private static final long serialVersionUID = 1L;
 	
+	
+	/** The logger. */
+	private static final Logger logger = Logger.getLogger(ManageCursoRealizadoControl.class.getCanonicalName());
+	
+	
+	/** The "Manage CursoRealizado" service. */
 	@EJB
 	private ManageCursoRealizadoService manageCursoRealizadoService;
 
+	
+	/** The controller de Egresso for get selected egresso*/ 
 	@Inject
 	private ManageEgressoControl manageEgressoControl;
+	
+	
+	
+	
+	
 	
 	
 	/*   CONSTRUTOR DA CLASSE */
@@ -28,20 +55,33 @@ public class ManageCursoRealizadoControl extends CrudController<CursoRealizado> 
 	     bundleName = "msgs";
 	}
 	
+	
+	
+	
+	/** @see br.ufes.inf.nemo.util.ejb3.controller.CrudController#getCrudService() */
 	@Override
 	protected CrudService<CursoRealizado> getCrudService() {
+		manageCursoRealizadoService.authorize();
 		return manageCursoRealizadoService;
 	}
 
+	
+	/** @see br.ufes.inf.nemo.util.ejb3.controller.CrudController#createNewEntity() */
 	@Override
 	protected CursoRealizado createNewEntity() {
+		logger.log(Level.FINER, "INITIALIZING AN EMPTY CURSOREALIZADO ......");
 		return new CursoRealizado();
 	}
-
+	
+	
+	/** @see br.ufes.inf.nemo.util.ejb3.controller.CrudController#initFilters() */
 	@Override
 	protected void initFilters() {
 	}
 	
+	
+	
+	/** @see br.ufes.inf.nemo.util.ejb3.controller.CrudController#create() */
 	@Override
 	public String create() {
 		readOnly = false;
