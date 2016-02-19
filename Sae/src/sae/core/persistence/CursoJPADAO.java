fce2ceeb94ea3baf564d4db72b3ecef4908fb4a7
manzoli2122 <manzoli2122@gmail.com>
2016-02-19 14:44:12 -0200

    atual

--- Sae/src/sae/core/persistence/CursoJPADAO.java
+++ Sae/src/sae/core/persistence/CursoJPADAO.java
@@ -1,11 +1,19 @@
 package sae.core.persistence;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
+
 import br.ufes.inf.nemo.util.ejb3.persistence.BaseJPADAO;
 import sae.core.domain.Curso;
+import sae.core.domain.Curso_;
 
 @Stateless
 public class CursoJPADAO extends BaseJPADAO<Curso> implements CursoDAO {
@@ -24,6 +32,14 @@ public class CursoJPADAO extends BaseJPADAO<Curso> implements CursoDAO {
 	@Override
 	protected EntityManager getEntityManager() {
 		return entityManager;
+	}
+	
+	
+	@Override
+	protected List<Order> getOrderList(CriteriaBuilder cb, Root<Curso> root) {
+		List<Order> orderList = new ArrayList<Order>();
+		orderList.add(cb.asc(root.get(Curso_.nome)));
+		return orderList;
 	}
 	
 	
